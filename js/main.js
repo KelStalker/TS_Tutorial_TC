22030105dabb618a83a6ef25e763fd6a4dbe9370
Kel <kelstalker@hotmail.com>
2020-04-02 10:55:42 -0600

    realtime compiler running - 2nd terminal open for github

--- js/main.js
+++
@@ -1,2 +0,0 @@
-// TS file extension indicates this is a TYPESCRIPT file
-console.log("hello crazy and confusing World!");
--- main.ts
+++ main.ts
@@ -1,2 +1,4 @@
 // TS file extension indicates this is a TYPESCRIPT file
-console.log("hello crazy and confusing World!");
+console.log("hello stupid people living in this crazy and confusing World!");
+console.log("Kiss my grits sunshine!");
+console.log("better work with Clayton watching");
