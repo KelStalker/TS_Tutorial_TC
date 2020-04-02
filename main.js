0e8c7a84e743f8c4bb3749b32fc9f258ff77e61e
Kel <kelstalker@hotmail.com>
2020-04-02 11:18:47 -0600

    live compiler working, dynamic variables created - no errors

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 console.log("hello stupid people living in this crazy and confusing World!");
 console.log("Kiss my grits sunshine!");
 console.log("better work with Clayton watching");
+var b = 1 + 2;
+var c = 'Hello-World!';
+var anotherOne = 1;
+anotherOne = 'string';
+anotherOne = {};
+anotherOne = true;
+console.log(b, c);
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -2,3 +2,13 @@
 console.log("hello stupid people living in this crazy and confusing World!");
 console.log("Kiss my grits sunshine!");
 console.log("better work with Clayton watching");
+
+const b = 1 + 2;
+const c = 'Hello-World!';
+
+var anotherOne = 1;
+anotherOne = 'string';
+anotherOne = {};
+anotherOne = true;
+
+console.log(b, c);
